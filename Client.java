4e5e565f0f4d0d5d6b344bad52188849672b9a13
Djordje Dimitrov <110050027+djolodjolo23@users.noreply.github.com>
2023-02-22 11:55:06 +0100

    Code logic moved to the Client. The server now creates the Clint thread and waits for the Thread to complete.

--- Client.java
+++ Client.java
@@ -3,12 +3,16 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.net.InetAddress;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class Client implements Runnable{
 
@@ -32,93 +36,204 @@ public class Client implements Runnable{
         "\r\n";
     this.header = new String(headerAsString.getBytes(), StandardCharsets.UTF_8);
   }
+
   public void run() {
     try {
       OutputStream output = clientSocket.getOutputStream();
-      String path = Server.getResource();
-      LocalDateTime currentDateTime = LocalDateTime.now();
-      DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-      String formattedDateTime = currentDateTime.format(formatter);
-      byte[] tempData = new byte[]{};
-      if (path.equals("/") || path.equals("/index.html")) {
-        String contentType = "text/html";
-        byte[] data = Files.readAllBytes(Path.of("public/index.html"));
-        try {
-          setHeader("HTTP/1.1 200 OK\r\n", data.length, contentType, formattedDateTime);
-          output.write(("HTTP/1.1 200 OK\r\n" +
-              "Content-Length: " + data.length + "\r\n" +
-              "Content-Type: " + contentType + "\r\n" +
-              "\r\n").getBytes());
-          output.write(data);
-        } catch (IOException e) {
-          setHeader("HTTP/1.1 404 Not Found\r\n", data.length, contentType, formattedDateTime);
-          output.write(("""
-              HTTP/1.1 404 Not Found\r
-              Content-Length: 0\r
-              \r
-              """).getBytes());
-        }
-      } else if (path.equals("/redirect")) {
-        try {
-          setHeader("HTTP/1.1 302 Found\r\n", tempData.length, "null", formattedDateTime);
-          output.write(("""
-              HTTP/1.1 302 Found\r
-              Location: /a\r
-              \r
-              """).getBytes());
-        } catch (IOException e) {
-          setHeader("HTTP/1.1 500 Internal Server Error\r\n", tempData.length, "null", formattedDateTime);
-          output.write(("""
-              HTTP/1.1 500 Internal Server Error\r
-              Content-Length: 0\r
-              \r
-              """).getBytes());
-        }
-      }
-      else {
-        Path filePath = Path.of("public" + path);
-        File file = filePath.toFile();
-        try {
-            if (!file.exists()) {
-              setHeader("HTTP/1.1 404 Not Found\r\n", tempData.length, "null", formattedDateTime);
-              output.write(("""
-                  HTTP/1.1 404 Not Found\r
-                  Content-Length: 0\r
-                  \r
-                  """).getBytes());
+      ArrayList<String> folders = new ArrayList<>();
+      // Create a new thread to handle the client connection
+      InputStreamReader inputStreamReader = new InputStreamReader(clientSocket.getInputStream());
+      BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+      String line;
+      if ((line = bufferedReader.readLine()) != null) {
+        String hostPort = bufferedReader.readLine();
+        if (!(hostPort == null)) {
+          String[] methodResourceVersion = line.split(" ");
+          //setResource(methodResourceVersion[1]);
+          if (!methodResourceVersion[1].endsWith(".html") && !methodResourceVersion[1].endsWith(".png")
+              && !methodResourceVersion[1].endsWith(".html/") && !methodResourceVersion[1].endsWith(".png/")) {
+            if (methodResourceVersion[1].charAt(methodResourceVersion[1].length() - 1) == '/'
+                || methodResourceVersion[1].charAt(methodResourceVersion[1].length() - 2) == '/') {
+              folders.add(methodResourceVersion[1]);
+            }
+          }
+            System.out.println("Assigned a new client to a separate thread!");
+            System.out.println(
+                hostPort + "," + " Method:" + methodResourceVersion[0] + ", Path: " + methodResourceVersion[1]
+                    + ", Version: " + methodResourceVersion[2]);
+            if (checkIfFileExists(methodResourceVersion[1])|| folders.contains(methodResourceVersion[1])) {
+              System.out.println("Requested file exists!");
+              if (folders.contains(methodResourceVersion[1])) {
+                System.out.println("Requested item is a folder.");
+              } else {
+                if (methodResourceVersion[1].endsWith(".html") || methodResourceVersion[1].endsWith(".html/")) {
+                  System.out.println("Requested item is an html file.");
+                } else if (methodResourceVersion[1].endsWith(".png") || methodResourceVersion[1].endsWith(".png/")) {
+                  System.out.println("Requested item is an png file.");
+                }
+              }
             } else {
-              String contentType = "text/plain";
-              if (path.endsWith(".html")) {
-                contentType = "text/html";
-              } else if (path.endsWith(".png")) {
-                contentType = "image/png";
-              }
-              if (file.isDirectory()) {
-                contentType = "text/html";
-                filePath = filePath.resolve("index.html");
-              }
-              byte[] data = Files.readAllBytes(Path.of(filePath.toUri()));
-              tempData = data;
+              System.out.println("Requested file does not exist!");
+            }
+          String path = methodResourceVersion[1];
+          LocalDateTime currentDateTime = LocalDateTime.now();
+          DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+          String formattedDateTime = currentDateTime.format(formatter);
+          byte[] tempData = new byte[]{};
+          if (path.equals("/") || path.equals("/index.html")) {
+            String contentType = "text/html";
+            byte[] data = Files.readAllBytes(Path.of("public/index.html"));
+            try {
               setHeader("HTTP/1.1 200 OK\r\n", data.length, contentType, formattedDateTime);
               output.write(("HTTP/1.1 200 OK\r\n" +
                   "Content-Length: " + data.length + "\r\n" +
                   "Content-Type: " + contentType + "\r\n" +
                   "\r\n").getBytes());
               output.write(data);
+            } catch (IOException e) {
+              setHeader("HTTP/1.1 404 Not Found\r\n", data.length, contentType, formattedDateTime);
+              output.write(("""
+                  HTTP/1.1 404 Not Found\r
+                  Content-Length: 0\r
+                  \r
+                  """).getBytes());
             }
-          } catch (IOException e) {
-            setHeader("HTTP/1.1 500 Internal Server Error\r\n", tempData.length, "null", formattedDateTime);
-          output.write(("""
-              HTTP/1.1 500 Internal Server Error\r
-              Content-Length: 0\r
-              \r
-              """).getBytes());
+          } else if (path.equals("/redirect")) {
+            try {
+              setHeader("HTTP/1.1 302 Found\r\n", tempData.length, "null", formattedDateTime);
+              output.write(("""
+                  HTTP/1.1 302 Found\r
+                  Location: /index.html\r
+                  \r
+                  """).getBytes());
+            } catch (IOException e) {
+              setHeader("HTTP/1.1 500 Internal Server Error\r\n", tempData.length, "null", formattedDateTime);
+              output.write(("""
+                  HTTP/1.1 500 Internal Server Error\r
+                  Content-Length: 0\r
+                  \r
+                  """).getBytes());
+            }
+          } else {
+            try {
+              if (validatePath(path)) {
+                output.write(("HTTP/1.1 404 Not Found\r\n" + "Content-Length: 0\r\n" +
+                    "\r\n").getBytes());
+              } else {
+                Path filePath = Path.of("public" + path);
+                File file = filePath.toFile();
+                if (!file.exists()) {
+                  setHeader("HTTP/1.1 404 Not Found\r\n", tempData.length, "null", formattedDateTime);
+                  output.write(("""
+                      HTTP/1.1 404 Not Found\r
+                      Content-Length: 0\r
+                      \r
+                      """).getBytes());
+                } else {
+                  String contentType = "text/plain";
+                  if (path.endsWith(".html")) {
+                    contentType = "text/html";
+                  } else if (path.endsWith(".png")) {
+                    contentType = "image/png";
+                  }
+                  if (file.isDirectory()) {
+                    contentType = "text/html";
+                    filePath = filePath.resolve("index.html");
+                  }
+                  byte[] data = Files.readAllBytes(Path.of(filePath.toUri()));
+                  tempData = data;
+                  setHeader("HTTP/1.1 200 OK\r\n", data.length, contentType, formattedDateTime);
+                  output.write(("HTTP/1.1 200 OK\r\n" +
+                      "Content-Length: " + data.length + "\r\n" +
+                      "Content-Type: " + contentType + "\r\n" +
+                      "\r\n").getBytes());
+                  output.write(data);
+                }
+              }
+            } catch (IOException e) {
+              setHeader("HTTP/1.1 500 Internal Server Error\r\n", tempData.length, "null", formattedDateTime);
+              output.write(("""
+                  HTTP/1.1 500 Internal Server Error\r
+                  Content-Length: 0\r
+                  \r
+                  """).getBytes());
+            }
+          }
+          InetAddress clientInetAddress = clientSocket.getInetAddress();
+          int port = clientSocket.getPort();
+          String header = getHeader();
+          if (!Objects.equals(header, "")) {
+            String[] headerArray = header.split("\r\n");
+            String response = headerArray[0].substring(8);
+            String dateTime = headerArray[3];
+            String contentLength = headerArray[1];
+            String contentType = headerArray[2];
+            InetAddress addr = InetAddress.getLocalHost();
+            String hostname = addr.getHostName();
+            clientSocket.close();
+            System.out.println(
+                "Client: " + clientInetAddress + port + ", Version: " + methodResourceVersion[2]
+                    + ", Response:" + response + ", " + dateTime + " \nServername:" + hostname + ", "
+                    + contentLength + ", " + checkIfSocketIsClosed(clientSocket)
+                    + ", " + contentType);
+            System.out.println("\n");
           }
         }
-      //clientSocket.close();
+      }
     } catch (IOException e) {
       e.printStackTrace();
     }
+  }
+
+  /**
+   * A method to ensure that the requested path is in 'public' directory.
+   */
+  private boolean validatePath(String path) {
+    Path publicPath = Paths.get("public").normalize();
+    Path requestedPath = publicPath.resolve(path).normalize();
+    return requestedPath.startsWith(publicPath);
+  }
+
+  /**
+   * A method for checking the socket status.
+
+   * @return is true or false.
+   */
+  private String checkIfSocketIsClosed(Socket socket) {
+    if (socket.isClosed()) {
+      return " Connection: closed";
+    } else {
+      return " Connection: open";
+    }
+  }
+
+
+  /**
+   * A method for checking if the file exists inside the public directory.
+   *
+   * @param fileName is the name of the file, or the resource.
+   * @return is true or false, depending on the outcome.
+   */
+  private boolean checkIfFileExists(String fileName) {
+    if (fileName.charAt(fileName.length() -1) == '/') {
+      if (fileName.length() != 1) {
+        fileName = fileName.substring(0, fileName.length() - 1);
+      }
+    }
+    String folderPath = fileName.substring(0, fileName.lastIndexOf('/'));
+    File folder = new File("public" + folderPath);
+    File[] files = folder.listFiles();
+    boolean doesTheFileExist = false;
+    if (files != null) {
+      for (File file : files) {
+        String fileNameWithoutFolder = folderPath + "/" + file.getName();
+        if (fileNameWithoutFolder.equals(fileName)) {
+          doesTheFileExist = true;
+          break;
+        }
+      }
+    }
+    return doesTheFileExist;
   }
 
 }
--- Server.java
+++ Server.java
@@ -11,9 +11,6 @@ import java.util.Objects;
 
 public class Server {
 
-  static String resource = "";
-
-
   public static void main(String[] args) {
     argsCheck(args);
     try (ServerSocket serverSocket = new ServerSocket(Integer.parseInt(args[0]))) {
@@ -27,67 +24,10 @@ public class Server {
         // client that's accepted
         // can accept multiple connections since in while(true) loop
         try (Socket clientSocket = serverSocket.accept()) {
-
-          ArrayList<String> folders = new ArrayList<>();
-          // Create a new thread to handle the client connection
-          InputStreamReader inputStreamReader = new InputStreamReader(clientSocket.getInputStream());
-          BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
           Client client = new Client(clientSocket);
-
-          String line;
-          if ((line = bufferedReader.readLine()) != null) {
-            String hostPort = bufferedReader.readLine();
-            if (!(hostPort == null)) {
-              String[] methodResourceVersion = line.split(" ");
-              setResource(methodResourceVersion[1]);
-              if (!methodResourceVersion[1].endsWith(".html") && !methodResourceVersion[1].endsWith(".png")) {
-                if (methodResourceVersion[1].charAt(methodResourceVersion[1].length() - 1) == '/'
-                    || methodResourceVersion[1].charAt(methodResourceVersion[1].length() - 2) == '/') {
-                  folders.add(methodResourceVersion[1]);
-                }
-              }
-              Thread clientThread = new Thread(client);
-              clientThread.start();
-              clientThread.join();
-              System.out.println("Assigned a new client to a separate thread!");
-              System.out.println(
-                  hostPort + "," + " Method:" + methodResourceVersion[0] + ", Path: " + methodResourceVersion[1]
-                      + ", Version: " + methodResourceVersion[2]);
-              if (checkIfFileExists(methodResourceVersion[1]) || folders.contains(methodResourceVersion[1])) {
-                System.out.println("Requested file exists!");
-                if (folders.contains(methodResourceVersion[1])) {
-                  System.out.println("Requested item is a folder.");
-                } else {
-                  if (methodResourceVersion[1].endsWith(".html")) {
-                    System.out.println("Requested item is an html file.");
-                  } else if (methodResourceVersion[1].endsWith(".png")) {
-                    System.out.println("Requested item is an png file.");
-                  }
-                }
-              } else {
-                System.out.println("Requested file does not exist!");
-              }
-              InetAddress clientInetAddress = clientSocket.getInetAddress();
-              int port = clientSocket.getPort();
-              String header = client.getHeader();
-              if (!Objects.equals(header, "")) {
-                String[] headerArray = header.split("\r\n");
-                String response = headerArray[0].substring(8);
-                String dateTime = headerArray[3];
-                String contentLength = headerArray[1];
-                String contentType = headerArray[2];
-                InetAddress addr = InetAddress.getLocalHost();
-                String hostname = addr.getHostName();
-                clientSocket.close();
-                System.out.println(
-                    "Client: " + clientInetAddress + port + ", Version: " + methodResourceVersion[2]
-                        + ", Response:" + response + ", " + dateTime + " \nServername:" + hostname + ", "
-                        + contentLength + ", " + checkIfSocketIsClosed(clientSocket)
-                        + ", " + contentType);
-                System.out.println("\n");
-                }
-              }
-            }
+          Thread clientThread = new Thread(client);
+          clientThread.start();
+          clientThread.join();
         } catch (IOException | InterruptedException e) {
           throw new RuntimeException(e);
         }
@@ -106,14 +46,6 @@ public class Server {
     }
   }
 
-  static String getResource() {
-    return resource;
-  }
-
-  static void setResource(String resource) {
-    Server.resource = resource;
-  }
-
   static void argsCheck(String[] args) {
     if (args.length != 2) {
       System.err.println("There must be two program arguments, the listening port and a relative folder path");
@@ -128,31 +60,5 @@ public class Server {
       System.err.println("the folder name should be 'public' ");
       System.exit(1);
     }
-  }
-
-  static String checkIfSocketIsClosed(Socket socket) {
-    if (socket.isClosed()) {
-      return " Connection: closed";
-    } else {
-      return " Connection: open";
-    }
-  }
-
-
-  static boolean checkIfFileExists(String fileName) {
-    String folderPath = fileName.substring(0, fileName.lastIndexOf('/'));
-    File folder = new File("public" + folderPath);
-    File[] files = folder.listFiles();
-    boolean doesTheFileExist = false;
-    if (files != null) {
-      for (File file : files) {
-        String fileNameWithoutFolder = folderPath + "/" + file.getName();
-        if (fileNameWithoutFolder.equals(fileName)) {
-          doesTheFileExist = true;
-          break;
-        }
-      }
-    }
-    return doesTheFileExist;
   }
 }
